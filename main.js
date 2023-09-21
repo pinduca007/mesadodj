97e1a1415534f9f8afb255a02571942f9386bd86
pinduca007 <130066449+pinduca007@users.noreply.github.com>
2023-09-21 10:36:36 -0300

    mesadodj

---
+++ main.js
@@ -0,0 +1,8 @@
+function tocaSom(idElementoAudio).play():
+ {
+  const listaDeTeclas = document.querySelectorAll(`.tecla`);
+  //para
+  for (let contador = 0; contador <listaDeTeclas.length; contador++)
+    
+  }
+}
